bb3b1a26130a1d88b49c07f3058c868a417652ce
Brett Chaldecott <brett.chaldecott@absa.co.za>
2019-08-13 09:29:13 -0400

    1) Added status checks to the docker compose scripts

--- build-dev/run_keto.sh
+++ build-dev/run_keto.sh
@@ -11,17 +11,17 @@ echo "Result ${setup_result} output ${setup_output}"
 echo "Confirm configration changes"
 cat /opt/keto/config/config.ini 
 
-echo "Tail the log files"
-tail -f /opt/keto/log/ketod_* &
-tail_pid=$!
-echo "Tail started with pid of ${tail_pid}"
+#echo "Tail the log files"
+#tail -f /opt/keto/log/ketod_* &
+#tail_pid=$!
+#echo "Tail started with pid of ${tail_pid}"
 
 echo "Start keto"
 /opt/keto/bin/ketod.sh &
 ketod_pid=$!
 echo "Keto started with a PID of ${ketod_pid}"
 
-pid_array=(${tail_pid} ${ketod_pid})
+pid_array=(${ketod_pid})
 
 for pid in ${pid_array[*]}; do
     echo "Wait for ${pid}"
--- scripts/dev_cluster.sh
+++ scripts/dev_cluster.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
+declare -a logs
+
+get_logs() {
+    echo -e "$(cd $COMPOSE_DIR && docker-compose logs)"
+}
+
 check_state() {
     echo "$(cd $COMPOSE_DIR && docker-compose top)"
+}
+
+parse_logs() {
+    local node=$1
+    local expression=$2
+    echo "$(echo -e "${logs[@]}" | grep "${node}" | grep "${expression}" | wc -l)"
+}
+
+node_check() {
+    local node=$1
+    network="$(parse_logs "${node}" "Network intialization is now complete")"
+    base_count=1
+    if [ "${network}" -gt "${base_count}" ] ; then
+        network="connected";
+    else
+        network="initializing";
+    fi
+    block_status="$(parse_logs "${node}" "Synchronization has now been completed")"
+    if [ "${block_status}" -gt "0" ] ; then
+        block_status="synchronized";
+    else
+        block_status="unsynchronized";
+    fi
+    echo "${node}-${network}-${block_status}"
+}
+
+check_info() {
+    logs="$(get_logs)"
+    node_check "master_1"
+    node_check "node1_1"
+    node_check "node2_1"
 }
 
 COMMAND=$1
@@ -14,6 +51,7 @@ then
     echo "  start - start the cluster."
     echo "  stop  - stop the cluster."
     echo "  status - status of the cluster."
+    echo "  info - info of the cluster."
     echo "  logs  - attach and print the logs."
     echo "  log   - log export for a node."
     echo "  clean - clean the containers up."
@@ -53,6 +91,9 @@ then
 elif [ "${COMMAND}" == "check" ]
 then
     echo -n "${CURRENT_STATE}"
+elif [ "${COMMAND}" == "info" ]
+then
+    check_info
 else
     cd ${COMPOSE_DIR} && docker-compose "$@"
 fi
