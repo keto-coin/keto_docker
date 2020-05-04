e056b74e45d0ee84b60d58a98b7465f5c9396a8a
Brett Chaldecott <brett.chaldecott@absa.co.za>
2020-05-04 14:41:09 +0200

    1) added version information to the debian build

--- scripts/debian_image.sh
+++ scripts/debian_image.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 IMAGE=$1
+VERSION_NUMBER=$2
 
 if [ -z "$IMAGE" ] ;
 then
-    echo "Must select the appropriate IMAGE"    
+    echo "Must select the appropriate debian action"
     echo "   build - the build image"
-    echo "   dev - the dev image"
+    exit -1
+elif [ -z "$VERSION_NUMBER" ] ;
+then
+    echo "Must select the appropriate debian version"
+    echo "   version - the version number"
     exit -1
 fi
 
@@ -16,5 +21,5 @@ COMPOSE_DIR=${WORK_DIR}/../compose-debian/
 if [ "${IMAGE}" == "build" ] ;
 then
     cd ${WORK_DIR}/../build-debian/ && docker build --no-cache -f Dockerfile . -t avertem/avertem-image
-    cd ${COMPOSE_DIR} && docker-compose up
+    cd ${COMPOSE_DIR} && VERSION_NUMER=$VERSION_NUMBER docker-compose up
 fi
